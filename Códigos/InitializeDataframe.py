07b926e2fa4b9477e761f55d5a8c513108f47977
Sheldron <sheldrongrandini@gmail.com>
2025-11-15 16:21:02 -0300

    Updating of functions on InitializeDataFrame file

--- Códigos/InitializeDataframe.py
+++ Códigos/InitializeDataframe.py
@@ -2,19 +2,21 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-def InitializeDataframe():
-    def LoadDataframe():
-        data = pd.read_csv(r'data\temporal_features_tsfel.csv')
-        label = pd.read_csv(r'data\label.csv')
+#dataPath: r'data\temporal_features_tsfel.csv'
+#labelPath: r'data\label.csv'
+def LoadDataframe(dataPath, labelPath):
+    data = pd.read_csv(dataPath)
+    label = pd.read_csv(labelPath)
             
-        return data, label
+    return data, label
 
-    def SplitDatabase(data, label, testSize):
-        x_train, x_test, y_train, y_test, = train_test_split(data, label, test_size = testSize)
+def SplitDatabase(data, label, testSize):
+    x_train, x_test, y_train, y_test, = train_test_split(data, label, test_size = testSize)
             
-        return x_train, x_test, y_train, y_test
+    return x_train, x_test, y_train, y_test
 
-    data, label = LoadDataframe()
+def InitializeDataframe(dataPath, labelPath):
+    data, label = LoadDataframe(dataPath, labelPath)
 
     x_train, x_test, y_train, y_test = SplitDatabase(data, label, testSize=0.20)
     y_test = np.ravel(y_test)
