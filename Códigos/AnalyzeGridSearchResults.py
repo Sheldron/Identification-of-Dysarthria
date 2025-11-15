bd5a0a2a12ce2339e8adfce9e8c3d082e51082bf
Sheldron <sheldrongrandini@gmail.com>
2025-11-15 18:09:21 -0300

    Optimizing funcrion organization

--- Códigos/AnalyzeGridSearchResults.py
+++ Códigos/AnalyzeGridSearchResults.py
@@ -1,48 +1,48 @@
 import pandas as pd
-from CtrFiles import ClearFile, OpenFile, CloseFile
+from CtrFiles import ClearFile, OpenFile, CloseFile, BreakLineFile
 
-def AnalyzeGridSearchResults(): 
-    def OpenDataFrame():
-        results = pd.read_csv(r'Resultados\ResultadosGrisSearch.csv')
+def OpenDataFrame():
+    results = pd.read_csv(r'Resultados\ResultadosGrisSearch.csv')
 
-        results = results.drop('index', axis=1)
+    results = results.drop('index', axis=1)
         
-        return results
+    return results
 
-    def SplitDfPerModel(df):
-        resultsKNN = df.loc[df['MODEL'] == "KNN"]
-        resultsSVM = df.loc[df['MODEL'] == "SVM"]
-        resultsRDF = df.loc[df['MODEL'] == "RDF"]
+def SplitDfPerModel(df):
+    resultsKNN = df.loc[df['MODEL'] == "KNN"]
+    resultsSVM = df.loc[df['MODEL'] == "SVM"]
+    resultsRDF = df.loc[df['MODEL'] == "RDF"]
 
-        resultsModels = [resultsKNN, resultsSVM, resultsRDF]
+    resultsModels = [resultsKNN, resultsSVM, resultsRDF]
 
-        return resultsModels
+    return resultsModels
 
-    def GetBestParams(df, scores):
-        f = OpenFile(pathFile)
-        f.write("Todas as informações de cada iteracao com os melhores parametros: \n")
+def GetBestParams(df, scores, pathFile):
+    f = OpenFile(pathFile)
+    f.write("Todas as informações de cada iteracao com os melhores parametros: \n")
         
-        for m in df:
-            for s in scores:
-                f.write(str(m.loc[m[s] == m[s].max()]))
+    for m in df:
+        for s in scores:
+            f.write(str(m.loc[m[s] == m[s].max()]))
         
-            f.write(f"\n===============================================\n")
+        BreakLineFile(f)
                 
-        CloseFile(f)
+    CloseFile(f)
         
-    def GetInfoScores(df, models, scores):
-        f = OpenFile(pathFile)
-        f.write("\nMedia e Desvio Padrao por metrica: \n\n")
+def GetInfoScores(df, models, scores, pathFile):
+    f = OpenFile(pathFile)
+    f.write("\nMedia e Desvio Padrao por metrica: \n\n")
         
-        for contador, m in enumerate(df):
-            for s in range(len(scores)):
-                f.write(str(f"A media de {scores[s]} para o modelo {models[contador]} eh {m[scores[s]].mean()} \n"))
-                f.write(str(f"O desvio padrao de {scores[s]} para o modelo {models[contador]} eh {m[scores[s]].std()} \n\n"))
+    for contador, m in enumerate(df):
+        for s in range(len(scores)):
+            f.write(str(f"A media de {scores[s]} para o modelo {models[contador]} eh {m[scores[s]].mean()} \n"))
+            f.write(str(f"O desvio padrao de {scores[s]} para o modelo {models[contador]} eh {m[scores[s]].std()} \n\n"))
             
-            f.write(f"\n===============================================\n")
+        BreakLineFile(f)
         
-        CloseFile(f)
+    CloseFile(f)
     
+def AnalyzeGridSearchResults(): 
     pathFile = r"Resultados\BestParamsPerModel.txt"
     
     ClearFile(pathFile)
@@ -54,8 +54,8 @@ def AnalyzeGridSearchResults():
     models = ["KNN", "SVM", "RDF"]
     scores = ["F1", "ACCURACY", "RECALL"]
     
-    GetBestParams(resultsModels, scores)
+    GetBestParams(resultsModels, scores, pathFile)
     
-    GetInfoScores(resultsModels, models, scores)
+    GetInfoScores(resultsModels, models, scores, pathFile)
 
 AnalyzeGridSearchResults()
--- Códigos/SaveTrainTest.py
+++ Códigos/SaveTrainTest.py
@@ -1,16 +1,16 @@
 import pandas as pd
 from InitializeDataframe import InitializeDataframe
 
+def SaveTrainTestToCSV(data, x_train, x_test, y_train, y_test):
+    df_train=pd.DataFrame(x_train,columns=data.columns)
+    df_train['Class'] = y_train
+    df_train.to_csv(r'DataTrain\data_train.csv',index=False)
+        
+    df_test=pd.DataFrame(x_test,columns=data.columns)
+    df_test['Class'] = y_test
+    df_test.to_csv(r'DataTrain\data_test.csv',index=False)
+
 def DefineTestAndTrainDF():
-    def SaveTrainTestToCSV(data, x_train, x_test, y_train, y_test):
-        df_train=pd.DataFrame(x_train,columns=data.columns)
-        df_train['Class'] = y_train
-        df_train.to_csv(r'DataTrain\data_train.csv',index=False)
-        
-        df_test=pd.DataFrame(x_test,columns=data.columns)
-        df_test['Class'] = y_test
-        df_test.to_csv(r'DataTrain\data_test.csv',index=False)
-        
     data, label, x_train, x_test, y_train, y_test = InitializeDataframe()
 
     SaveTrainTestToCSV(data, x_train, x_test, y_train, y_test)
